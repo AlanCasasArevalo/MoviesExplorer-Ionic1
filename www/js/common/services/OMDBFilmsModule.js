221a41245c32b28df45929309ff3496c223ba47d
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2018-06-15 11:35:44 +0200

    OmdbFilmsModule was modified to get filmNames, omdbApi  to get from title a valid url to ask for response to the server constants were added to our factory films array, films service, selected film, urlFromTitle and selectFilmByTitle were added

--- www/js/common/services/OMDBFilmsModule.js
+++ www/js/common/services/OMDBFilmsModule.js
@@ -2,47 +2,75 @@
     'use strict';
 
     angular.module('OMDBFilmsModule', ['FilmModel'])
-        .factory('FilmsService', function($http, Film) {
 
-            var filmsService = {};
+    .constant('filmNames', [
+        'the Martian',
+        'interstellar',
+        'Book Club',
+        'Hereditary',
+        'Hotel Artemis',
+        'Upgrade',
+        'Solo: A Star Wars Story',
+        'Deadpool 2'
+    ])
 
-            filmsService.films = [];
+    .constant('omdbAPi', (function() {
+        var namePlaceholder = ['namePlaceholder'];
 
-            filmsService.selectedFilm = null;
+        return {
+            url: 'http://www.omdbapi.com/?t=' + namePlaceholder + '&y=&plot=short&r=json',
+            namePlaceholder: namePlaceholder
+        };
 
-            var selecFilmByTitle = function(title) {
-                for (var index = 0; index < filmsService.length; index++) {
-                    if (filmsService.films[index].title === title) {
-                        return filmsService.films[index];
-                    }
+    })())
+
+    .factory('FilmsService', function($http, $q, Film, filmNames, omdbAPi) {
+
+        var filmsService = {};
+
+        filmsService.films = [];
+
+        filmsService.selectedFilm = null;
+
+        var urlFromTitle = function(title) {
+            var titleQueryString = title.split(' ').join('+');
+            var url = omdbAPi.url.replace(omdbAPi.namePlaceholder, titleQueryString);
+            return url;
+        };
+
+        var selecFilmByTitle = function(title) {
+            for (var index = 0; index < filmsService.length; index++) {
+                if (filmsService.films[index].title === title) {
+                    return filmsService.films[index];
                 }
+            }
 
-                return null;
-            };
+            return null;
+        };
 
-            filmsService.getSpecificFilm = function(title) {
-                var deferred = $q.defer();
+        filmsService.getSpecificFilm = function(title) {
+            var deferred = $q.defer();
 
 
-                if (filmsService.films.length > 0) {
-                    filmsService.selectedFilm = selecFilmByTitle(title);
+            if (filmsService.films.length > 0) {
+                filmsService.selectedFilm = selecFilmByTitle(title);
+                deferred.resolve(this.filmsService.selectedFilm);
+            } else {
+                $http.get(urlFromTitle(title), {}).then(function(success) {
+                    filmsService.selectedFilm = Film.build(response.data);
                     deferred.resolve(this.filmsService.selectedFilm);
-                } else {
-                    $http.get('url', {}).then(function(success) {
-                        filmsService.selectedFilm = Film.build(response.data);
-                        deferred.resolve(this.filmsService.selectedFilm);
-                    }, function(error) {
-                        filmsService.selectedFilm = null;
-                        deferred.resolve(null);
-                    });
-                }
+                }, function(error) {
+                    filmsService.selectedFilm = null;
+                    deferred.resolve(null);
+                });
+            }
 
-                return deferred.promise;
-            };
+            return deferred.promise;
+        };
 
-            filmsService.getAllFilms = function() {};
+        filmsService.getAllFilms = function() {};
 
-            return filmsService;
-        });
+        return filmsService;
+    });
 
 })();
