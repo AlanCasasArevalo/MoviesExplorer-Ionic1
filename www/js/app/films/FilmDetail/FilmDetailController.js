06e1d51f3107a176cfab3e2585d2c13167cfb922
Alan Casas Arevalo <alancasasarevalo@gmail.com>
2018-06-16 09:29:15 +0200

    FilmDetailController was modified to use sliderbox instead normal page film-detail.html was modified to add slider

--- www/js/app/films/FilmDetail/FilmDetailController.js
+++ www/js/app/films/FilmDetail/FilmDetailController.js
@@ -3,15 +3,40 @@
 
     angular.module('FilmModule')
 
-    .controller('FilmDetailController', function($scope, Film, currentFilmIndex) {
+    .controller('FilmDetailController', function($scope, FilmsService, currentFilmIndex) {
+
+        $scope.data = {};
+        $scope.data.films = FilmsService.films;
 
         var initViewWithContent = function() {
             $scope.currentFilmPage = currentFilmIndex;
         };
 
+
+        var setupSlider = function() {
+            $scope.data.sliderOptions = {
+                initialSlide: currentFilmIndex,
+                direction: 'horizontal',
+                speed: 300,
+                grabCursor: true
+            };
+            $scope.data.sliderDelegate = null;
+
+            $scope.$watch('data.sliderDelegate', function(newVal, oldVal) {
+                if (newVal != null) {
+                    $scope.data.sliderDelegate.on('slideChangeEnd', function() {
+                        $scope.data.currentFilmPage = $scope.data.sliderDelegate.activeIndex;
+                        $scope.$apply();
+                    });
+                }
+            });
+        };
+
         $scope.$on('$ionicView.loaded', function() {
             initViewWithContent();
         });
+
+        setupSlider();
     });
 
 })();
